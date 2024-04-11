dd23c5ce0c3e68bd27520f9cbd72fda603148489
Vinicius Ribeiro <157416457+Vineco77@users.noreply.github.com>
2024-04-11 12:47:11 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function alterarStatus(id) {
+  let gameClicado = document.getElementById(`game-${id}`);
+  let imagem = gameClicado.querySelector(".dashboard__item__img");
+  let botao = gameClicado.querySelector(".dashboard__item__button");
+
+  if (botao.classList.contains("dashboard__item__button--return")) {
+    botao.classList.remove("dashboard__item__button--return");
+    botao.textContent = "Alugar";
+    imagem.classList.remove("dashboard__item__img--rented");
+  } else {
+    botao.classList.add("dashboard__item__button--return");
+    botao.textContent = "Devolver";
+    imagem.classList.add("dashboard__item__img--rented");
+  }
+}
